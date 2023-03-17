13738e2da05462c316b11648071f1cb285d2ca42
elaifa <mouezelaifa@gmail.com>
2023-03-17 15:14:13 -0700

    Task 2. I sometimes suffer from insomnia. And when I can"t fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-    char letter;
+char letter;
 
-    for (letter = 'a'; letter <= 'z'; letter++)
+for (letter = 'a'; letter <= 'z'; letter++)
     {
     /* Print each letter of the alphabet */
     putchar(letter);
     }
     /* Print a newline character at the end */
-    putchar('\n');
-    return (0);
+putchar('\n');
+return (0);
 }
