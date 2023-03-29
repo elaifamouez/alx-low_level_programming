8dc6106084c76dcd5185503f780b21547c4c3d60
elaifa <mouezelaifa@gmail.com>
2023-03-29 15:50:05 -0700

    Task 6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,30 +6,32 @@
  *
  * Return: the resulting string
  */
+#include "main.h"
+
+/**
+ * cap_string - capitalizes everey word of a string
+ * @s: string to modify
+ *
+ * Return: the resulting string
+ */
 char *cap_string(char *str)
 {
-int i;
-unsigned int j;
-int capitalize_next = 1;
-char separators[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+int i, j;
+char spe[13] = {' ', '\t', '\n', ',', ';', '.','!', '?', '"', '(', ')', '{', '}'};
 
 for (i = 0; str[i] != '\0'; i++)
 {
-for (j = 0; j < sizeof(separators) / sizeof(separators[0]); j++)
+if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+str[i] -= 32;
+for (j = 0; j < 13; j++)
 {
-if (str[i] == separators[j])
+if (str[i] == spe[j])
 {
-capitalize_next = 1;
-break;
+if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+{
+str[i + 1] -= 32;
 }
 }
-if (capitalize_next)
-{
-if (str[i] >= 'a' && str[i] <= 'z')
-{
-str[i] = str[i] - 'a' + 'A';
-}
-capitalize_next = 0;
 }
 }
 return (str);
