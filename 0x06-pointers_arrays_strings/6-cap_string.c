0aa8eebc73f1c6fa55d9b2d7b397967cb9e31cc0
elaifa <mouezelaifa@gmail.com>
2023-03-29 16:01:37 -0700

    Task 6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,30 +1,31 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes everey word of a string
- * @s: string to modify
+ * cap_string - capitalizes all words of a string
+ * @s: input string.
  *
- * Return: the resulting string
+ * Return: the pointer to dest.
  */
 char *cap_string(char *str)
 {
-int i, j;
-char spe[13] = {' ', '\t', '\n', ',', ';', '.','!', '?', '"', '(', ')', '{', '}'};
+int count = 0, i;
+int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-for (i = 0; str[i] != '\0'; i++)
+if (*(str + count) >= 97 && *(str + count) <= 122)
+*(str + count) = *(str + count) - 32;
+count++;
+while (*(str + count) != '\0')
 {
-if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
-str[i] -= 32;
-for (j = 0; j < 13; j++)
+for (i = 0; i < 13; i++)
 {
-if (str[i] == spe[j])
+if (*(str + count) == sep_words[i])
 {
-if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-{
-str[i + 1] -= 32;
+if ((*(str + (count + 1)) >= 97) && (*(str + (count + 1)) <= 122))
+*(str + (count + 1)) = *(str + (count + 1)) - 32;
+break;
 }
 }
-}
+count++;
 }
 return (str);
 }
