d26665ca9c7473c28f2a4401f1532b321de77a0d
elaifa <mouezelaifa@gmail.com>
2023-03-29 12:05:48 -0700

    Task 0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,13 +10,14 @@
  * Return: pointer to resulting string @dest
 */
 
-char* _strcat(char* dest, char* src) {
-
+char* _strcat(char* dest, char* src)
+{
 char* ptr = dest + strlen(dest); 
 
-while (*src != '\0') {
+while (*src != '\0')
+{
 *ptr++ = *src++; 
 }
 *ptr = '\0';
-return dest;
+return (dest);
 }
