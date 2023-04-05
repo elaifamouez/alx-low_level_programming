4c98ae5bd9c9c55f45244080c0938342d05c38b7
elaifa <mouezelaifa@gmail.com>
2023-04-05 14:36:40 -0700

    Task 2. Dreams feel real while we"re in them. It"s only when we wake up that we realize something was actually strange

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,14 +9,5 @@
  */
 int _strlen_recursion(char *s)
 {
-if (s == NULL)
-return (0);
-if (*s)
-{
-return (1 + _strlen_recursion(s + 1));
-}
-else
-{
-return (0);
-}
+return (*s == '\0' ? 0 : (_strlen_recursion(s + 1) + 1));
 }
