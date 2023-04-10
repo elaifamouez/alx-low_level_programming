a62a3d4c5aebef75b590611325d322deeb9a2c7f
elaifa <mouezelaifa@gmail.com>
2023-04-10 15:07:12 -0700

    Task 4. To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - adds positive numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ *
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
+{
+int i, sum = 0;
+
+for (i = 1; i < argc; i++)
+{
+char *arg = argv[i];
+while (*arg)
+{
+if (!isdigit(*arg))
+{
+printf("Error\n");
+return (1);
+}
+arg++;
+}
+sum += atoi(argv[i]);
+}
+return (sum ? printf("%d\n", sum) : printf("0\n"));
+}
