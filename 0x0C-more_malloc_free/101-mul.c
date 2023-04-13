720e6b8371fbb96e16bb2ad3c8f2b0e22f7ba308
elaifa <mouezelaifa@gmail.com>
2023-04-13 16:12:30 -0700

    Task 5. We must accept finite disappointment, but never lose infinite hope

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
+ */
+ int main(int argc, char *argv[])
+{
+char *s1, *s2;
+int l1, l2, l, i, carry, d1, d2, *res, a = 0;
+
+s1 = argv[1], s2 = argv[2];
+if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+errors();
+l1 = _strl(s1);
+l2 = _strl(s2);
+l = l1 + l2 + 1;
+res = malloc(sizeof(int) * l);
+if (!res)
+return (1);
+i = 0;
+while (i <= l1 + l2)
+{
+res[i] = 0;
+i++;
+}
+for (l1 = l1 - 1; l1 >= 0; l1--)
+{
+d1 = s1[l1] - '0';
+carry = 0;
+for (l2 = _strl(s2) - 1; l2 >= 0; l2--)
+{
+d2 = s2[l2] - '0';
+carry += res[l1 + l2 + 1] + (d1 * d2);
+res[l1 + l2 + 1] = carry % 10;
+carry /= 10;
+}
+if (carry > 0)
+res[l1 + l2 + 1] += carry;
+}
+i = 0;
+while (i < l - 1)
+{
+if (res[i])
+a = 1;
+if (a)
+_putchar(res[i] + '0');
+i++;
+}
+if (!a)
+_putchar('0');
+_putchar('\n');
+free(res);
+return (0);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,8 +1,49 @@
 #ifndef MAIN_H
 #define MAIN_H
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+/**
+ * _strl - returns the lgth of a string
+ * @s: string to evaluate
+ *
+ * Return: the lgth of the string
+ */
+int _strl(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+return (i);
+}
+/**
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_digit(char *s)
+{
+int i = 0;
+while (s[i])
+{
+if (s[i] < '0' || s[i] > '9')
+return (0);
+i++;
+}
+return (1);
+}
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
 #endif/*MAIN_H*/
