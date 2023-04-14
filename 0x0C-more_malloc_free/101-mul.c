7445e4cdf13b1b30371ae6c1d78e1efa1bfdf81c
elaifa <mouezelaifa@gmail.com>
2023-04-13 18:26:54 -0700

    Task 5. We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -61,14 +61,9 @@ int l1, l2, l, i, carry, d1, d2, *res, a = 0;
 s1 = argv[1], s2 = argv[2];
 if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 errors();
-if (*s1 == '0' || *s2 == '0')
-{
-_putchar('0');
-_putchar('\n');
-}
 l1 = _strl(s1);
 l2 = _strl(s2);
-l = l1 + l2 + 1;
+l = l1 + l2;
 res = malloc(sizeof(int) * l);
 if (!res)
 return (1);
