d44b1c52363eb908e0e2219d5caf920a053fa48b
elaifa <mouezelaifa@gmail.com>
2023-04-13 19:22:31 -0700

    Task 5. We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -36,14 +36,6 @@ i++;
 return (i);
 }
 /**
- * errors - handles errors for main
- */
-void errors(void)
-{
-	printf("Error\n");
-	exit(98);
-}
-/**
  * main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
@@ -54,10 +46,17 @@ int main(int argc, char *argv[])
 {
 char *s1, *s2;
 int l1, l2, l, i, carry, d1, d2, *res, a = 0;
+char E[6] = {'E', 'r', 'r', 'o', 'r', '\n'};
 
 s1 = argv[1], s2 = argv[2];
 if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-errors();
+{
+for (i = 0; i < 6; i++)
+{
+_putchar(E[i]);
+}
+exit(98);
+}
 l1 = _strl(s1);
 l2 = _strl(s2);
 l = l1 + l2 + 1;
