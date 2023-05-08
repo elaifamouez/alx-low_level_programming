2d59c9ac57fe37ce81a2f60b1b295cd55e46d9f7
elaifa <mouezelaifa@gmail.com>
2023-05-08 10:30:04 -0700

    Task 2. Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,7 +10,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-register int _file, _write, len;
+register int _file, _write, len = 0;
 
 if (filename == NULL)
 return (-1);
