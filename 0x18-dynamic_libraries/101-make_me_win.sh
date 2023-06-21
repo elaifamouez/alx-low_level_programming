9d808b8bdcf48b1ed6f494cbfb4801f4f4e27ba7
elaifa <mouezelaifa@gmail.com>
2023-06-21 13:03:20 -0700

    0x18. C - Dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q --output-document=$HOME/lib101-gm_overhaul.so https://github.com/elaifamouez/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lib101-gm_overhaul.so
+wget -q --output-document=$HOME/lib101-gm_overhaul.so https://github.com/elaifamouez/alx-low_level_programming/tree/master/0x18-dynamic_libraries/lib101-gm_overhaul.so
 export LD_PRELOAD=$HOME/lib101-gm_overhaul.so
